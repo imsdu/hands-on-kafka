8fe1133b053cf7c64c127f24c92c8328bd29c15c
Fred CECILIA <fred@morijaonline.com>
2017-04-02 00:29:07 +0200

    Add stateless exo using branch

--- hands-on-java/README.md
+++ hands-on-java/README.md
@@ -2,7 +2,9 @@
 - Create a topic with [key, value] =>  [ID COMMIT,CONTRIBUTOR]
 - Create a topics related about a fix and another for the others [ID COMMIT,COMMENT]
 - Create a topic with [key, value] =  [ID CONTRIBUTOR,CONTRIBUTOR INFO]
+- Create 2 topic with [key, value] =  [ID ,COMMIT] with commit relate to a fix and not
 - Create POJO pour user and a topic with [key, value] =  [ID USER,USER]
+
 
 ## Stateful transformations- Cecilia
 - With a table with with a count of commit by contributor
---
+++ hands-on-java/src/main/java/fr/devoxx/kafka/streams/exos/transformations/stateless/BranchCommit.java
@@ -0,0 +1,70 @@
+package fr.devoxx.kafka.streams.exos.transformations.stateless;
+
+import fr.devoxx.kafka.conf.AppConfiguration;
+import fr.devoxx.kafka.streams.pojo.GitMessage;
+import fr.devoxx.kafka.streams.pojo.serde.PojoJsonSerializer;
+import fr.devoxx.kafka.utils.AppUtils;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Hands-on kafka streams Devoxx 2017
+ */
+public class BranchCommit {
+
+    private static final String APP_ID = AppUtils.appID("BranchCommit");
+
+    public static void main(String[] args) {
+
+        // Create an instance of StreamsConfig from the Properties instance
+        StreamsConfig config = new StreamsConfig(AppConfiguration.getProperties(APP_ID));
+        final Serde<String> stringSerde = Serdes.String();
+        final Serde<Long> longSerde = Serdes.Long();
+
+        Map<String, Object> serdeProps = new HashMap<>();
+
+        final PojoJsonSerializer<GitMessage> jsonSerializer = new PojoJsonSerializer<>();
+        serdeProps.put("PojoJsonSerializer", GitMessage.class);
+        jsonSerializer.configure(serdeProps, false);
+
+        final Serde<GitMessage> messageSerde = Serdes.serdeFrom(jsonSerializer, jsonSerializer);
+
+        KStreamBuilder kStreamBuilder = new KStreamBuilder();
+
+        //START EXO
+
+        KStream<String, GitMessage> messagesStream =
+                kStreamBuilder.stream(stringSerde, messageSerde, AppConfiguration.SCALA_GITLOG_TOPIC)
+                        .map((k, v) -> KeyValue.pair(v.getHash(), v));
+
+        KStream<String, GitMessage>[] commit = messagesStream
+                .branch(
+                        (key, value) -> value.getMessage().toLowerCase().contains("fix"),
+                        (key, value) -> !value.getMessage().toLowerCase().contains("fix")
+                );
+
+
+        commit[0].to(stringSerde, messageSerde, "CommitFixStream");
+        commit[1].to(stringSerde, messageSerde, "CommitFeatureStream");
+
+        //STOP EXO
+
+
+        System.out.println("Starting Kafka Streams Gitlog Example");
+        KafkaStreams kafkaStreams = new KafkaStreams(kStreamBuilder, config);
+        kafkaStreams.cleanUp();
+        kafkaStreams.start();
+        System.out.println("Now started Gitlog Example");
+
+    }
+
+
+}
