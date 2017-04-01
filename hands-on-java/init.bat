48b284f4829414ac6841ce3f5b0a9dd06ccd5614
Fred CECILIA <fred@morijaonline.com>
2017-04-01 20:18:37 +0200

    Init.bat

---
+++ hands-on-java/init.bat
@@ -0,0 +1,4 @@
+mvn exec:java -Dexec.mainClass="fr.devoxx.kafka.producer.InitProducer" -Dexec.args="..\data\users.json users"
+mvn exec:java -Dexec.mainClass="fr.devoxx.kafka.producer.InitProducer" -Dexec.args="..\data\scala-gitlog.json scala-gitlog"
+mvn exec:java -Dexec.mainClass="fr.devoxx.kafka.producer.InitProducer" -Dexec.args="..\data\contributors.json contributors"
+mvn exec:java -Dexec.mainClass="fr.devoxx.kafka.producer.InitProducer" -Dexec.args="..\data\all-users.json all-users"
