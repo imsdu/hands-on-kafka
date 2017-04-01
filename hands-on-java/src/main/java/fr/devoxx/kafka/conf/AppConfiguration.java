85de0e7b1cd66e6aed6285a572055cde0466870d
Fred CECILIA <fred@morijaonline.com>
2017-04-01 20:59:36 +0200

    Fix SCHEMA_REGISTRY

--- hands-on-java/src/main/java/fr/devoxx/kafka/conf/AppConfiguration.java
+++ hands-on-java/src/main/java/fr/devoxx/kafka/conf/AppConfiguration.java
@@ -8,11 +8,11 @@ import java.util.Properties;
 /**
  * Created by fred on 01/04/2017.
  */
-public  class AppConfiguration {
+public class AppConfiguration {
 
     public static String URL_BASE = "192.168.99.100"; //"localhost"
-    public static String BOOTSTRAP_SERVERS_CONFIG = URL_BASE+ ":9092";
-    public static String SCHEMA_REGISTRY =  "http://"+URL_BASE+ ":8081";
+    public static String BOOTSTRAP_SERVERS_CONFIG = URL_BASE + ":9092";
+    public static String SCHEMA_REGISTRY = "http://" + URL_BASE + ":8081";
     public static String STATE_DIR_CONFIG = "/tmp";
     public static String AUTO_OFFSET_RESET_CONFIG = "earliest";
 
@@ -25,7 +25,7 @@ public  class AppConfiguration {
         settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfiguration.BOOTSTRAP_SERVERS_CONFIG);
 
         settings.put(StreamsConfig.STATE_DIR_CONFIG, AppConfiguration.STATE_DIR_CONFIG);
-        settings.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,  AppConfiguration.AUTO_OFFSET_RESET_CONFIG);
+        settings.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, AppConfiguration.AUTO_OFFSET_RESET_CONFIG);
         return settings;
     }
 }
