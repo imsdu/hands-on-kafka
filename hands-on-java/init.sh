4936d5ae02a344c035400f325f5aeeded02babb0
Fred CECILIA <fred@morijaonline.com>
2017-04-01 22:10:48 +0200

    fix refactor

--- hands-on-java/init.sh
+++ hands-on-java/init.sh
@@ -1,5 +1,5 @@
 mvn compile
-mvn exec:java -Dexec.mainClass="fr.devoxx.kafka.producer.InitProducer" -Dexec.args="../data/users.json users"
-mvn exec:java -Dexec.mainClass="fr.devoxx.kafka.producer.InitProducer" -Dexec.args="../data/scala-gitlog.json scala-gitlog"
-mvn exec:java -Dexec.mainClass="fr.devoxx.kafka.producer.InitProducer" -Dexec.args="../data/contributors.json contributors"
-mvn exec:java -Dexec.mainClass="fr.devoxx.kafka.producer.InitProducer" -Dexec.args="../data/all-users.json all-users"
+mvn exec:java -Dexec.mainClass="fr.devoxx.kafka.client.InitProducer" -Dexec.args="../data/users.json users"
+mvn exec:java -Dexec.mainClass="fr.devoxx.kafka.client.InitProducer" -Dexec.args="../data/scala-gitlog.json scala-gitlog"
+mvn exec:java -Dexec.mainClass="fr.devoxx.kafka.client.InitProducer" -Dexec.args="../data/contributors.json contributors"
+mvn exec:java -Dexec.mainClass="fr.devoxx.kafka.client.InitProducer" -Dexec.args="../data/all-users.json all-users"
